c21af33beb03c7e9164333c923ea422fdfcb6417
jkelly8823 <jkelly8823@gmail.com>
2024-09-19 13:16:51 -0500

    First groq tests and analysis

---
+++ analyzers/output_parser.py
@@ -0,0 +1,59 @@
+import os
+import pandas as pd
+import re
+import dotenv
+from pathlib import Path
+
+dotenv.load_dotenv()
+
+def main():
+    fail_log = []
+
+    cols = ['TRIAL', 'PROMPT', 'MODEL', 'SOURCE', 'DATA_ID', 'FILENAME', 'VULN_MODEL', 'VULN_TRUTH']
+    df = pd.DataFrame(columns=cols)
+
+    for root, dirs, files in os.walk(os.getenv('parser_dir')):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            path_parts = Path(file_path).parts
+
+            with open(file_path, 'r+', encoding='utf-8', errors='replace') as file:
+                output = file.read()
+
+            # Define the regex pattern to match both cases
+            pattern = r"VULNERABLE.*:.*(YES)|(NO)"
+
+            # Search for matches using regex
+            matches = re.findall(pattern, output)
+
+            llmVuln = -1
+            for match in matches:
+                if match[0]:  # Group 1 (YES)
+                    llmVuln = True
+                    break
+                elif match[1]:  # Group 2 (NO)
+                    llmVuln = False
+                    break
+            if (llmVuln == -1):
+                fail_log.append(f'Could not match vulnerability status in: {file_path}\n')
+                continue
+            
+            # True Vuln Status
+            trueVuln = True
+            # print(path_parts)
+            if ('cwe' in path_parts[4].lower()) and ('good' in path_parts[6].lower()):
+                trueVuln = False
+
+            # New row data as a dictionary
+            new_row = {'TRIAL':path_parts[1], 'PROMPT':path_parts[2], 'MODEL':path_parts[3], 'SOURCE':path_parts[4], 'DATA_ID':path_parts[5], 'FILENAME':path_parts[6], 'VULN_MODEL':llmVuln, 'VULN_TRUTH':trueVuln}
+
+            # Add the new row
+            df.loc[len(df)] = new_row
+
+    df.to_csv('analyzers/parsed_outputs.csv', index=True, index_label='INDEX')
+
+    with open("analyzers/parser_fail_log.txt", 'w+') as file:
+        file.writelines(fail_log)
+
+if __name__ == "__main__":
+    main()
--- groq/groq_runner.py
+++ groq/groq_runner.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import dotenv
 from groq import Groq
 import re
@@ -32,22 +33,45 @@ def call_groq(msg_content, model_name):
         )
 
         # print(chat_completion.choices[0].message.content)
-        os.makedirs(f'groq_outputs/{model_name}/{i[0]}', exist_ok=True)
-        write_resp(f'groq_outputs/{model_name}/{i[0]}/{i[1]}.txt',chat_completion.choices[0].message.content)
+        os.makedirs(f'groq_outputs/{os.getenv("test_dir")}/prompt_{i[3]}/{model_name}/{i[0]}', exist_ok=True)
+        write_resp(f'groq_outputs/{os.getenv("test_dir")}/prompt_{i[3]}/{model_name}/{i[0]}/{i[1]}.txt',chat_completion.choices[0].message.content)
 
+def get_prompts(dir):
+    messages = {}
+    for root, dirs, files in os.walk(dir):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            with open(file_path, 'r+', encoding='utf-8') as file:
+                msg = file.read()
+                messages[os.path.splitext(filename)[0]] = msg
+                
+    return messages
 
 def main():
     # Prompt
-    msg= os.getenv('groq_prompt')
+    msgs = get_prompts(os.getenv('prompt_dir'))
 
     # Limit the number of before/after file pairs to evaluate
     limit = int(os.getenv('groq_limit'))
 
     # Define the directory of samples you want to iterate through
-    directory = './samples'
+    directory = os.getenv('sample_dir')
 
     # Define the models to run on groq
     models = json.loads(os.getenv('groq_models'))
+
+    # print('-------------------')
+    # print(msgs)
+    # print('-------------------')
+    # print(next(iter(msgs)) + ':\n' + msgs[next(iter(msgs))])
+    # print('-------------------')
+    # print(limit)
+    # print('-------------------')
+    # print(directory)
+    # print('-------------------')
+    # print(models)
+    # print('-------------------')
+    # return
 
     # Recursively iterate through the directory
     content = []
@@ -61,15 +85,19 @@ def main():
             match = re.search(r'([^\\]+\\[^\\]+)$', root)
             # print(f'root: {root}')
             # print(f'dir: {match.group(1)}')
-            content.append([match.group(1), filename, msg + code_sample])
-            
-            cnt += 0.5
+            if ('attack' in filename.lower()) and ('cwe' in file_path.lower()):
+                continue
+            for key, value in msgs.items():
+                content.append([match.group(1), filename, value + '\n\n' + code_sample, key])
+                
+            cnt += 1
             if cnt == limit:
                 break
         if cnt == limit:
             break
 
     for model in models:
+        print(f'Running model: {model}')
         call_groq(content, model)
 
 if __name__ == "__main__":
